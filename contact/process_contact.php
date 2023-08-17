d3ed8220d81068f3f249eedc9c08d099e82665ea
frd-of-moon <70507132+frd-of-moon@users.noreply.github.com>
2023-08-17 23:23:07 +0530

    contact php

--- contact/process_contact.php
+++ contact/process_contact.php
@@ -4,14 +4,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    $to = "charis.rhea@gmail.com"; 
-    $subject = "Contact Form Submission";
-    $headers = "From: $email";
+    require 'PHPMailer/PHPMailerAutoload.php';
 
-    if (mail($to, $subject, $message, $headers)) {
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 587;
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'tls';
+    $mail->Username = 'frdofmoon@gmail.com'; // Your Gmail email address
+    $mail->Password = 'Rofl6060'; // Your Gmail password
+    $mail->setFrom('your@gmail.com', 'Your Name');
+    $mail->addAddress('recipient@example.com'); // Recipient's email address
+    $mail->Subject = 'Contact Form Submission';
+    $mail->Body = "Name: $name\nEmail: $email\nMessage: $message";
+
+    if ($mail->send()) {
         echo "Message sent successfully.";
     } else {
-        echo "Error sending message.";
+        echo "Error sending message: " . $mail->ErrorInfo;
     }
 }
 ?>
