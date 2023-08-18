60c1e6ee66e56c1e18fb9ec9031f5774b63dda13
varshacb <83687662+varshacb@users.noreply.github.com>
2023-08-18 22:18:01 +0530

    minor changes

--- app.py
+++ app.py
@@ -202,24 +202,6 @@ def registration_success():
 
 
 
-
-    # return render_template('index.html')
-
-
-
-@app.route("/registration-success", methods=['GET','POST'])
-def registration_success():
-    if (request.method == "POST"):
-        staffincharge=request.form.get("institution")
-        staffemail=request.form.get("email")
-        mobile=request.form.get("mobile_number")
-
-        collection_name='staff-details'
-        collection=database[collection_name]
-
-
-
-
 if __name__ == '__main__':
 
     app.run(debug=True, host='0.0.0.0', port=PORT)
